db01d018ab9ac638ad53f1501a547a3ea652d866
Andrey Lebedev <alebedev@redhat.com>
2024-06-04 11:13:00 +0200

    add vars to policies

--- aws-cmds.sh
+++ aws-cmds.sh
@@ -9,13 +9,15 @@ cp -f albo-controller-trusted-policy.json albo-controller-trusted-policy.json.in
 sed -i "s|\${IDP}|${IDP}|" albo-operator-trusted-policy.json.in
 sed -i "s|\${IDP}|${IDP}|" albo-controller-trusted-policy.json.in
 
-aws iam create-role --role-name albo-operator --assume-role-policy-document file://albo-operator-trusted-policy.json
+aws iam delete-role-policy --role-name albo-operator --policy-name perms-policy-albo-operator
+aws iam delete-role --role-name albo-operator
+
+aws iam create-role --role-name albo-operator --assume-role-policy-document file://albo-operator-trusted-policy.json.in
 aws iam put-role-policy --role-name albo-operator --policy-name perms-policy-albo-operator --policy-document file://albo-operator-permission-policy.json
 
-aws iam create-role --role-name albo-controller --assume-role-policy-document file://albo-controller-trusted-policy.json
+aws iam delete-role-policy --role-name albo-controller --policy-name perms-policy-albo-controller
+aws iam delete-role --role-name albo-controller
+
+aws iam create-role --role-name albo-controller --assume-role-policy-document file://albo-controller-trusted-policy.json.in
 aws iam put-role-policy --role-name albo-controller --policy-name perms-policy-albo-controller --policy-document file://albo-controller-permission-policy.json
 
-# aws iam delete-role-policy --role-name albo-operator --policy-name perms-policy-albo-operator
-# aws iam delete-role --role-name albo-operator
-# aws iam delete-role-policy --role-name albo-controller --policy-name perms-policy-albo-controller
-# aws iam delete-role --role-name albo-controller
